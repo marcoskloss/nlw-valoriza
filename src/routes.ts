9f59bed60d942aee79bf32c435d9a8edbbbbb629
marcoskloss <marcoskloss2001@gmail.com>
2021-06-23 19:31:18 -0300

    add create tag route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
+import CreateTagController from './controllers/CreateTagController'
 import CreateUserController from './controllers/CreateUserController'
+import { ensureAdmin } from './middlewares/ensureAdmin'
 
 const router = Router()
 
 router.post('/users', CreateUserController.handle)
+router.post('/tags', ensureAdmin, CreateTagController.handle)
 
 export { router }
