89cffc3f5c5e48e265c7e07f4b2d3882b2ce665d
marcoskloss <marcoskloss2001@gmail.com>
2021-06-22 21:43:17 -0300

    add create user route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import 'reflect-metadata'
-import express from 'express'
 import './database'
 
+import { router } from './routes'
+import express from 'express'
+
+const PORT = 3000
 const app = express()
 
-app.listen(3000, () => {
-  console.log('Server is running')
+app.use(express.json())
+app.use(router)
+ 
+app.listen(PORT, () => {
+  console.log('Server is running 🔥')
 })
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import { getCustomRepository } from "typeorm"
+import { User } from "../entities/User"
+import { UserRepository } from "../repositories/UserRepository"
+
+export interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({ name, email, admin }: IUserRequest): Promise<User> {
+    if (!email) {
+      throw new Error('Email incorrect')
+    }
+    
+    const userRepository = getCustomRepository(UserRepository)
+    console.log(userRepository)
+    const userAlreadyExists = await userRepository.findOne({
+      email
+    })
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists')
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+      admin
+    })
+
+    userRepository.save(user)
+    return user
+  }
+} 
+
+export { CreateUserService }
