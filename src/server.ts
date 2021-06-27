e8677b6202249645da57c92598e55543b578ee74
marcoskloss <marcoskloss2001@gmail.com>
2021-06-26 21:29:25 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,20 @@ import 'reflect-metadata'
 import './database'
 import 'express-async-errors'
 import { router } from './routes'
+import cors from 'cors'
 import express, { NextFunction, Request, Response } from 'express'
 
 const PORT = 3000
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 app.use(router)
  
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
+    console.log('Erro: ', err.message)
+    console.log('Stack: ', err.stack)
     return res.status(400).json({ error: err.message })
   } 
 
